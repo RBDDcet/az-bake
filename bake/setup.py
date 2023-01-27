3f23fc468e9248af21dd0425d89296f2d81a409c
Roger Best <rbest@microsoft.com>
2023-01-26 16:17:27 -0800

    Iteration Q

--- bake/setup.py
+++ bake/setup.py
@@ -16,7 +16,7 @@ except ImportError:
     from distutils import log as logger
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
 
-# Must match a HISTORY.rst entry. Test P
+# Must match a HISTORY.rst entry. Test Q
 VERSION = '0.3.11'
 
 # The full list of classifiers is available at
