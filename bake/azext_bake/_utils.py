40d2051e0a224993ea67b1302b0ddd2a40cbe04d
Roger Best <rbest@microsoft.com>
2023-02-01 16:00:15 -0800

    Fix missing dash iteration BD

--- bake/azext_bake/_utils.py
+++ bake/azext_bake/_utils.py
@@ -201,7 +201,7 @@ def get_choco_package_setup(package: ChocoPackage) -> str:
         if key.__eq__("id"):
             choco_setup_string += f"{pkg[key]}"
         else:
-            choco_setup_string += f" -{key} '{pkg[key]}'"
+            choco_setup_string += f" --{key} '{pkg[key]}'"
 
     choco_setup_string += ' --yes --no-progress'
     return choco_setup_string
